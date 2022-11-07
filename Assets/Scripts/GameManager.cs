f03834ee32d5d0722a1692d35b0198a9111061f7
ahnw00 <ahnw00@naver.com>
2022-11-08 01:15:48 +0900

    GameManager(setting)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [HideInInspector] public bool playerArrivedEndPoint = false;
+    [HideInInspector] public bool player1Turn = false;
+    [HideInInspector] public bool player2Turn = false;
+    int round = 0;
+    [SerializeField] Image cuttonImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(TurnManager());
+        player1Turn = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TurnManager()
     {
-        
+        yield return null;
+        while(!playerArrivedEndPoint)
+        {
+            while(player1Turn)
+            {
+                yield return null;
+            }
+
+            cuttonImage.enabled = true;
+
+            while(player2Turn)
+            {
+                yield return null;
+            }
+        }
     }
 }
