e018d65868ca493992667c2333566abe76c5290e
Jinius36 <63860923+Jinius36@users.noreply.github.com>
2022-11-17 22:00:32 +0900

    assignment 1117
    comment CardSlot.cs
    develop NextTurnBtn.cs (incomplete)

--- Assets/Scripts/CardSlot.cs
+++ Assets/Scripts/CardSlot.cs
@@ -7,7 +7,7 @@ public class CardSlot : MonoBehaviour
     [SerializeField] Card cardOnSlot;
     [HideInInspector] public int slotNum;
     public bool isThereCardOnSlot = false;
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col) // 카드와 슬롯이 만날 때
     {
         if(!isThereCardOnSlot)
         {
@@ -18,7 +18,7 @@ public class CardSlot : MonoBehaviour
         }
     }
 
-    void OnTriggerExit2D(Collider2D col)
+    void OnTriggerExit2D(Collider2D col) // 카드가 슬롯에서 벗어날 때
     {
         if(col.GetComponent<Card>() == cardOnSlot)
         {
--- Assets/Scripts/NextTurnBtn.cs
+++ Assets/Scripts/NextTurnBtn.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class NextTurnBtn : MonoBehaviour
 {
-    public GameObject player1panel, player2panel;
+    public GameObject Player1Panel, Player2Panel;
     public SlotManager slotManager;
     // Start is called before the first frame update
     void Start()
     {
         slotManager = FindObjectOfType<SlotManager>();
     }
+
+    public void ClickNextTurnBtn()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            cuttonImage.SetActive(true); // 커튼 이미지의 inspector에 접근하는 법? serializedfield라 접근이 안 되나?
+            Player1Panel.SetActive(false);
+            Player2Panel.SetActive(true);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -20,11 +31,11 @@ public class NextTurnBtn : MonoBehaviour
 
     public void TestBtn() // for slotManger func test. do not discard
     {
-        if(player1panel.activeSelf)
+        if(Player1Panel.activeSelf)
         {
             slotManager.CheckP1SlotNum();
         }
-        else if(player2panel.activeSelf)
+        else if(Player2Panel.activeSelf)
         {
             slotManager.CheckP2SlotNum();
         }
