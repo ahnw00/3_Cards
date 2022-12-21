d02a33436801be481790a5e1f75554950b50dd97
Jinius36 <63860923+Jinius36@users.noreply.github.com>
2022-12-21 20:53:10 +0900

    setactive(false)

--- Assets/Scripts/Panel/NextTurnBtn.cs
+++ Assets/Scripts/Panel/NextTurnBtn.cs
@@ -8,10 +8,8 @@ public class NextTurnBtn : MonoBehaviour
     GameManager gameManager;
     public GameObject player1Panel, player2Panel;
     [SerializeField] bool slot0, slot1, slot2;
-    [SerializeField] int Player1slot0Num, Player1slot1Num, Player1slot2Num;
-    [SerializeField] int Player2slot0Num, Player2slot1Num, Player2slot2Num;
     [SerializeField] SlotManager slotManager;
-    public GameObject[] player1CardDeck;
+    public GameObject[] player1CardDeck, player2CardDeck;
 
     // Start is called before the first frame update
     void Start()
@@ -31,48 +29,47 @@ public class NextTurnBtn : MonoBehaviour
             if(player1Panel.activeSelf)
             {
                 slotManager.CheckP1SlotNum();
-                Player1slot0Num = GameObject.Find("Slot (0)").GetComponent<CardSlot>().slotNum;
-                Player1slot1Num = GameObject.Find("Slot (1)").GetComponent<CardSlot>().slotNum;
-                Player1slot2Num = GameObject.Find("Slot (2)").GetComponent<CardSlot>().slotNum;
-                
                 string num1 = slotManager.player1Card[0].ToString();
                 string num2 = slotManager.player1Card[1].ToString();
+                string num3 = slotManager.player1Card[2].ToString();
                 
                 for(int i = 0; i < 6; i++) // 카드 덱 리스트에 들어있는 모든 카드 순회
                 {
-                    if(player1CardDeck[i].tag == num1 || player1CardDeck[i].tag == num2) // 슬롯에 올려져있는 카드의 넘버와 일치한다면
+                    if(player1CardDeck[i].tag == num1 || player1CardDeck[i].tag == num2 || player1CardDeck[i].tag == num3) // 슬롯에 올려져있는 카드의 넘버와 일치한다면
                     {
                         player1CardDeck[i].SetActive(false); // 해당카드 꺼줌
                     }
                 }
-                GameObject.Find("Player1Panel").transform.GetChild(Player1slot0Num).gameObject.SetActive(false);
-                GameObject.Find("Player1Panel").transform.GetChild(Player1slot1Num).gameObject.SetActive(false);
-                GameObject.Find("Player1Panel").transform.GetChild(Player1slot2Num).gameObject.SetActive(false);
                 gameManager.player1Turn = false;
                 player1Panel.SetActive(false);
             }
             else if(player2Panel.activeSelf)
             {
                 slotManager.CheckP2SlotNum();
+                string num1 = slotManager.player2Card[0].ToString();
+                string num2 = slotManager.player2Card[1].ToString();
+                string num3 = slotManager.player2Card[2].ToString();
+                
+                for(int i = 0; i < 6; i++) // 카드 덱 리스트에 들어있는 모든 카드 순회
+                {
+                    if(player2CardDeck[i].tag == num1 || player2CardDeck[i].tag == num2 || player2CardDeck[i].tag == num3) // 슬롯에 올려져있는 카드의 넘버와 일치한다면
+                    {
+                        player2CardDeck[i].SetActive(false); // 해당카드 꺼줌
+                    }
+                }
                 gameManager.player2Turn = false;
                 gameManager.showingResult = true;
                 player2Panel.SetActive(false);
-                Player2slot0Num = GameObject.Find("Slot (0)").GetComponent<CardSlot>().slotNum;
-                Player2slot1Num = GameObject.Find("Slot (1)").GetComponent<CardSlot>().slotNum;
-                Player2slot2Num = GameObject.Find("Slot (2)").GetComponent<CardSlot>().slotNum;
-                GameObject.Find("Player1Panel").transform.GetChild(Player2slot0Num).gameObject.SetActive(false);
-                GameObject.Find("Player1Panel").transform.GetChild(Player2slot1Num).gameObject.SetActive(false);
-                GameObject.Find("Player1Panel").transform.GetChild(Player2slot2Num).gameObject.SetActive(false);
             }
         }
 
     }
 
-    void SetOffCard(int slotNumber, string slot, string panel)
+    /*void SetOffCard(int slotNumber, string slot, string panel)
     {
         slotNumber = GameObject.Find(slot).GetComponent<CardSlot>().slotNum;
         GameObject.Find(panel).transform.GetChild(slotNumber).gameObject.SetActive(false);
-    }
+    }*/
 
     public void TestBtn() // for slotManger func test. do not discard
     {
