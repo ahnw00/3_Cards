57b060f6f5b45dd247670d9144e85a4ce2a2dc54
ahnw00 <ahnw00@naver.com>
2023-01-16 21:39:20 +0900

    anim

---
+++ Assets/Scripts/InGame/LoadingManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingManager : MonoBehaviour
+{
+    [SerializeField] Text resultText;
+    [SerializeField] Text resultText1;
+    [SerializeField] Text resultText2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimationEvent()
+    {
+        //만약 플레이어1 승리시 resultText.text = "플레이어1 승리";
+    }
+}
