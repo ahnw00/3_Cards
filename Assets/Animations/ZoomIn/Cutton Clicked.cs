0f16df45f7e917b1c99e9508da3e6c24d0f33f08
Jinius36 <63860923+Jinius36@users.noreply.github.com>
2023-01-25 04:14:02 +0900

    Zoom In

---
+++ Assets/Animations/ZoomIn/Cutton Clicked.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CuttonClicked : MonoBehaviour
+{
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        GameObject cutton = null;
+        animator = GetComponent<Animator>();
+        cutton = GameObject.Find("Cutton");
+
+        if (cutton.activeSelf == true)
+        {
+            animator.SetBool("Cutton Clicked", false);
+        }
+        else (cutton.activeSelf == false)
+        {
+            animator.SetBool("Cutton Clicked", true);
+        }
+    
+    }
+}
