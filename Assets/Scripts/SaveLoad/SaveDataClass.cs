0ecb058163c217168b6c161de0edcbcf8003005b
doyeonk429 <doyeonk429@gmail.com>
2023-01-28 21:10:53 +0900

    Add new scripts

---
+++ Assets/Scripts/SaveLoad/SaveDataClass.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveDataClass
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public SaveDataClass()
+    {
+        
+    }
+
+}
